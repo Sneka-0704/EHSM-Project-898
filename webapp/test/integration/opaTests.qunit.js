79416b755a0d72a8a048b320b84133e236da2f6a
SNEKA VARSHINI V <snekavarshini07@gmail.com>
2026-01-13 09:16:52 +0000

    basic template for ehsm

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["ehsm898/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ehsm898/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
